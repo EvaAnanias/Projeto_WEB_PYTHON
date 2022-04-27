d8235d8a459b949751bd6f4bb9105339d8de6379
EvaAnanias <evamariananias@gmail.com>
2022-04-26 23:05:27 -0300

    update

--- calendario/views.py
+++ calendario/views.py
@@ -1,10 +1,9 @@
 from django.http import HttpResponse
-
-# from django.shortcuts import render
+from django.shortcuts import render
 
 
 def home(request):
-    return HttpResponse('Home 1')
+    return render(request, 'home.html')
 
 
 def cadastro(request):
