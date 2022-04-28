792a108d3f70019c2862eb17314259c9d83b4d0d
EvaAnanias <evamariananias@gmail.com>
2022-04-28 18:24:09 -0300

    update

--- calendario/views.py
+++ calendario/views.py
@@ -2,12 +2,12 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'calendario/home.html')
+    return render(request, 'calendario/pages/home.html')
 
 
 def cadastro(request):
-    return render(request, 'calendario/cadastro.html')
+    return render(request, 'calendario/pages/cadastro.html')
 
 
 def calendario(request):
-    return render(request, 'calendario/calendario.html')
+    return render(request, 'calendario/pages/calendario.html')
