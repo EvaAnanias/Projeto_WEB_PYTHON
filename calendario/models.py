098b3b149f2274f455a9633fa599392eecd08bcc
EvaAnanias <evamariananias@gmail.com>
2022-05-04 13:47:14 -0300

    uptade

--- calendario/models.py
+++ calendario/models.py
@@ -4,16 +4,35 @@ from django.db import models
 # Create your models here.
 
 
-#class Category(models.Model):  nome = models.CharField(max_length=65)
+# class Category(models.Model):  nome = models.CharField(max_length=65)
 
 
 class calendarioForm(forms.Form):
-    nome_compromisso = models.CharField(max_length=65)
-    data_compromisso = models.DateTimeField(auto_now_add=True)
-    local = models.CharField(max_length=80)
-    hora_inicio = models.IntegerField()
-    hora_termino = models.IntegerField()
-    message = models.TextField(blank=True, null=True)
+
+    nome = forms.CharField(
+        label='nome_compromisso',
+        max_length=65)
+
+    nome = forms.DateTimeField(
+        label='data_compromisso',
+        auto_now_add=True)
+
+    nome = forms.CharField(
+        label='nome_compromisso',
+        max_length=65)
+
+    nome = forms.CharField(
+        label='nome_compromisso',
+        max_length=65)
+
+    nome = forms.CharField(
+        label='nome_compromisso',
+        max_length=65)
+
+    nome = forms.CharField(
+        label='nome_compromisso',
+        max_length=65)
+
     # category = models.ForeignKey(
     #   Category, on_delete=models.SET_NULL, null=True
     # )
@@ -23,4 +42,9 @@ class calendarioForm(forms.Form):
 
 
 class calendario(models.Model):
-    pass
+    nome_compromisso = models.CharField(max_length=65)
+    data_compromisso = models.DateTimeField(auto_now_add=True)
+    local = models.CharField(max_length=80)
+    hora_inicio = models.IntegerField()
+    hora_termino = models.IntegerField()
+    message = models.TextField(blank=True, null=True)
